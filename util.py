c0b3a8a78e195680498f9ae5c648358c06e565c0
Nico-Merck <nico.merck@uni-rostock.de>
2025-10-21 15:16:44 +0200

    Updated package dependencies

--- util.py
+++ util.py
@@ -1,18 +1,7 @@
 import numpy as np
 import pandas as pd
 
-from matplotlib.lines import Line2D
-
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-from sklearn.pipeline import Pipeline
-from sklearn.cluster import KMeans
-from sklearn.metrics import accuracy_score, roc_auc_score, precision_score, silhouette_score, silhouette_samples, confusion_matrix, ConfusionMatrixDisplay, recall_score, f1_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier, NearestCentroid
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import StratifiedKFold, train_test_split
+from sklearn.metrics import accuracy_score, roc_auc_score, precision_score, confusion_matrix, ConfusionMatrixDisplay, recall_score, f1_score
 
 def evaluate_classifier(clf, X_train, X_test, y_train, y_test, labels, plot=False):
     clf.fit(X_train, y_train)
